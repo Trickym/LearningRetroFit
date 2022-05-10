57ce2ce60872fa4f6a24c6e74edc03947c5ccd72
Prashant Mishra <pm21641@gmail.com>
2022-05-10 10:12:08 +0530

    recycler view

---
+++ app/src/main/java/com/example/retrofitdemo/AdapterData.kt
@@ -0,0 +1,34 @@
+package com.example.retrofitdemo
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterData(private val dataList : List<DemoDataItem>):RecyclerView.Adapter<AdapterData.ViewHolderData>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdapterData.ViewHolderData {
+        val demoDataItem = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item,parent,false)
+        return  ViewHolderData(demoDataItem)
+    }
+
+    override fun onBindViewHolder(holder: AdapterData.ViewHolderData, position: Int) {
+        val currentDataItem = dataList[position]
+        holder.userId.text = "User Id : ${currentDataItem.userId}"
+        holder.dataId.text = "Data Id : ${currentDataItem.id}"
+        holder.title.text = "Title : ${currentDataItem.title}"
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    inner class ViewHolderData(dataItem: View):RecyclerView.ViewHolder(dataItem) {
+        val userId:TextView = dataItem.findViewById(R.id.user_id)
+        val dataId:TextView = dataItem.findViewById(R.id.data_id)
+        val title : TextView = dataItem.findViewById(R.id.title)
+
+    }
+
+}
--- app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -3,6 +3,9 @@
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -11,15 +14,15 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.lang.StringBuilder
 
     class MainActivity : AppCompatActivity() {
-        private lateinit var tv:TextView
+        private var recyclerView: RecyclerView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tv = findViewById(R.id.txt_view)
+        recyclerView= findViewById(R.id.recycler_view)
         getDemoData()
     }
 
-    private fun getDemoData() {
+    private fun  getDemoData(){
         val retrofitBuilder = Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl("https://jsonplaceholder.typicode.com/")
@@ -33,18 +36,13 @@ import java.lang.StringBuilder
                     response: Response<List<DemoDataItem>?>
                 ) {
                     val responseBody = response.body()!!
-                    for(each in responseBody){
-                        string.append("Title : ${each.title}")
-                        string.append("\n")
-                        string.append("---------------------")
-                        string.append("\n")
-
-                    }
-                    tv.text = string
+                    recyclerView!!.adapter = AdapterData(responseBody)
+                    recyclerView!!.layoutManager = LinearLayoutManager(this@MainActivity)
+                    recyclerView!!.setHasFixedSize(true)
                 }
 
                 override fun onFailure(call: Call<List<DemoDataItem>?>, t: Throwable) {
-                    TODO("Not yet implemented")
+                    Toast.makeText(this@MainActivity,"Something went wrong!",Toast.LENGTH_SHORT).show()
                 }
             })
     }
